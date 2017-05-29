f6f4d87efe83b662f377f9e5f4c2a7f25287d5a0
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-29 13:22:39 +0200

    add __about__.py

---
+++ code_extract/__about__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+__version__ = '0.1.0'
+__author__ = u'Nico Schlömer'
+__author_email__ = 'nico.schloemer@gmail.com'
+__status__ = 'Development Status :: 3 - Alpha'
+__license__ = 'License :: OSI Approved :: MIT License'

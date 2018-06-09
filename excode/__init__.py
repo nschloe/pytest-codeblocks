fc04dbaf6d1541ea4d21cfbacd671daca7c2c16e
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-09 19:50:17 +0200

    lint fixes

--- excode/__init__.py
+++ excode/__init__.py
@@ -2,5 +2,6 @@
 #
 from .__about__ import __version__, __author__, __author_email__
 
-# pylint: disable=wildcard-import
-from .main import *
+from .main import extract, write
+
+__all__ = ["extract", "write", "__version__", "__author__", "__author_email__"]

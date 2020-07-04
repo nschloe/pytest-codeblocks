511069f96dbd9d5a4a7bf273dd8b0997b48195d0
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-04 10:25:08 +0200

    remove unused code

--- excode/main.py
+++ excode/main.py
@@ -1,9 +1,4 @@
 import re
-import textwrap
-
-
-def indent(text, amount, ch=" "):
-    return textwrap.indent(text, amount * ch).replace("\n    \n", "\n\n")
 
 
 def from_file(f, filter=None):
@@ -29,34 +24,3 @@ def from_string(f, filter=None):
             code_blocks.append("".join(code_block[:-1]))
 
     return code_blocks
-
-
-def write(f, code_blocks, prefix="test"):
-    # We'd like to put all code blocks in one file, each in separate test*()
-    # functions (for them to be picked up by pytest, for example), but
-    # asterisk imports are forbidden in subfunctions. Hence, parse for those
-    # imports and put them at the beginning of the output file.
-    asterisk_imports = []
-    clean_code_blocks = []
-    for code_block in code_blocks:
-        clean_code_block = []
-        for line in code_block.split("\n"):
-            if re.match("\\s*from\\s+[^\\s]+\\s+import\\s+\\*", line):
-                asterisk_imports.append(line)
-            else:
-                clean_code_block.append(line)
-        clean_code_blocks.append("\n".join(clean_code_block))
-    # make list unique
-    asterisk_imports = list(set(asterisk_imports))
-
-    if asterisk_imports:
-        f.write("\n".join(asterisk_imports))
-        f.write("\n\n")
-
-    fun_strings = []
-    for k, code_block in enumerate(clean_code_blocks):
-        fun_strings.append("")
-        fun_strings[-1] += f"def {prefix}{k}():\n"
-        fun_strings[-1] += indent(code_block, 4)
-        fun_strings[-1] += "    return\n"
-    f.write("\n\n".join(fun_strings))

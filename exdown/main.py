92d68d36ddcce7e804fbcd787cfbcba2f810b22e
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-11 17:46:28 +0200

    return line numbers, too

--- exdown/main.py
+++ exdown/main.py
@@ -7,28 +7,33 @@ def extract(f, *args, **kwargs):
 
 
 def from_buffer(f, syntax_filter=None):
-    code_blocks = []
+    out = []
     previous_line = None
+    k = 1
     while True:
         line = f.readline()
+        k += 1
         if not line:
             # EOF
             break
 
-        out = re.match("[^`]*```(.*)$", line)
-        if out:
+        m = re.match("[^`]*```(.*)$", line)
+        if m:
+            lineno = k - 1
             # read the block
             code_block = [f.readline()]
+            k += 1
             while re.search("```", code_block[-1]) is None:
                 code_block.append(f.readline())
+                k += 1
 
-            if syntax_filter and syntax_filter.strip() != out.group(1).strip():
+            if syntax_filter and syntax_filter.strip() != m.group(1).strip():
                 continue
             if previous_line.strip() == "<!--exdown-skip-->":
                 continue
 
-            code_blocks.append("".join(code_block[:-1]))
+            out.append(("".join(code_block[:-1]), lineno))
 
         previous_line = line
 
-    return code_blocks
+    return out

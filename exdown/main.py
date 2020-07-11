0eac0f38f84bba12ecbf62a9d106a7498dba942d
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-11 17:54:22 +0200

    add max_num_lines to prevent failure

--- exdown/main.py
+++ exdown/main.py
@@ -6,7 +6,7 @@ def extract(f, *args, **kwargs):
         return from_buffer(handle, *args, **kwargs)
 
 
-def from_buffer(f, syntax_filter=None):
+def from_buffer(f, max_num_lines=10000, syntax_filter=None):
     out = []
     previous_line = None
     k = 1
@@ -24,8 +24,13 @@ def from_buffer(f, syntax_filter=None):
             code_block = [f.readline()]
             k += 1
             while re.search("```", code_block[-1]) is None:
+                print("a", k)
                 code_block.append(f.readline())
                 k += 1
+                if k > max_num_lines:
+                    raise RuntimeError(
+                        f"File too large (> {max_num_lines} lines). Set max_num_lines."
+                    )
 
             if syntax_filter and syntax_filter.strip() != m.group(1).strip():
                 continue

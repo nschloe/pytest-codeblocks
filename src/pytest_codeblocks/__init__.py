b649d922da56432587838e6a4f25f0b5f4f49d1a
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-07 13:05:04 +0200

    remove unused __about__

--- src/pytest_codeblocks/__init__.py
+++ src/pytest_codeblocks/__init__.py
@@ -1,9 +1,7 @@
 from . import plugin
-from .__about__ import __version__
 from .main import CodeBlock, extract_from_buffer, extract_from_file
 
 __all__ = [
-    "__version__",
     "CodeBlock",
     "extract_from_buffer",
     "extract_from_file",

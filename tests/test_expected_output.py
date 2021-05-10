17b5f3bc3908489c3ddd1635f6f48ed2997889da
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-10 10:53:20 +0200

    fix for expected code block

--- tests/test_expected_output.py
+++ tests/test_expected_output.py
@@ -6,18 +6,24 @@ string = """
 Lorem ipsum
 ```python
 print(1 + 3)
+print(1 - 3)
+print(1 * 3)
 ```
 dolor sit amet
 <!--pytest-codeblocks:expected-output-->
 ```
 4
+-2
+3
 ```
 """
 
 
-# test_frombuffer = pytest_codeblocks.pytests_from_buffer(io.StringIO(string))
-
-
 def test_cont():
     lst = pytest_codeblocks.extract_from_buffer(io.StringIO(string))
-    assert lst == [pytest_codeblocks.CodeBlock("print(1 + 3)\n", 3, "python", "4\n")]
+    print(lst)
+    assert lst == [
+        pytest_codeblocks.CodeBlock(
+            "print(1 + 3)\nprint(1 - 3)\nprint(1 * 3)\n", 3, "python", "4\n-2\n3\n"
+        )
+    ]

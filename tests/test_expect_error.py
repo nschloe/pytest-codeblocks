7d44509c0dd7ea6dd7fbfe216a371b10a661e553
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-18 19:48:20 +0200

    refine tests

--- tests/test_expect_error.py
+++ tests/test_expect_error.py
@@ -1,18 +1,24 @@
-string1 = """
-Lorem ipsum
-<!--pytest-codeblocks:expect-exception-->
-```python
-raise RuntimeError()
-```
-dolor
-<!--pytest-codeblocks:expect-exception-->
-```python
-1 + 1
-```
-"""
-
-
 def test_expect_error(testdir):
+    string = """
+    Lorem ipsum
+    <!--pytest-codeblocks:expect-exception-->
+    ```python
+    raise RuntimeError()
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
+
+
+def test_expect_error_fail(testdir):
+    string1 = """
+    Lorem ipsum
+    <!--pytest-codeblocks:expect-exception-->
+    ```python
+    1 + 1
+    ```
+    """
     testdir.makefile(".md", string1)
     result = testdir.runpytest("--codeblocks")
-    result.assert_outcomes(passed=1, failed=1)
+    result.assert_outcomes(failed=1)

10db4cdec76ba5f02f94c9c650c03e835ee18919
Nico Schlömer <nico.schloemer@gmail.com>
2021-05-25 09:50:20 +0200

    add test for hidden cont

--- tests/test_cont.py
+++ tests/test_cont.py
@@ -15,6 +15,25 @@ def test_cont(testdir):
     result.assert_outcomes(passed=1)
 
 
+def test_hidden_cont(testdir):
+    string = """
+    Lorem ipsum
+    <!--
+    ```python
+    a = 1
+    ```
+    -->
+    dolor sit amet
+    <!--pytest-codeblocks:cont-->
+    ```
+    a + 1
+    ```
+    """
+    testdir.makefile(".md", string)
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=1)
+
+
 def test_nocont(testdir):
     string = """
     <!--pytest-codeblocks:cont-->

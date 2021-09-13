0c257f7a96b48f3dc05ce2c9007cad47cf446d3e
Nico Schlömer <nico.schloemer@gmail.com>
2021-09-13 20:47:08 +0200

    make utf-8 the default encoding

--- src/pytest_codeblocks/main.py
+++ src/pytest_codeblocks/main.py
@@ -20,7 +20,7 @@ class CodeBlock:
 
 
 def extract_from_file(
-    f: Union[str, bytes, Path], encoding: Optional[str] = None, *args, **kwargs
+    f: Union[str, bytes, Path], encoding: Optional[str] = "utf-8", *args, **kwargs
 ):
     with open(f, encoding=encoding) as handle:
         return extract_from_buffer(handle, *args, **kwargs)

915f8f6fdd54f505e15771662bb6e3f2e1d31697
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2021-10-08 12:29:45 +0200

    linting regex

--- src/pytest_codeblocks/main.py
+++ src/pytest_codeblocks/main.py
@@ -70,7 +70,7 @@ def extract_from_buffer(f, max_num_lines: int = 10000):
 
             # check for keywords
             m = re.match(
-                "<!--\-?\s?pytest-codeblocks:(.*)\-?\s?-->",
+                r"<!--\-?\s?pytest-codeblocks:(.*)\-?\s?-->",
                 previous_nonempty_line.strip(),
             )
             if m is None:
